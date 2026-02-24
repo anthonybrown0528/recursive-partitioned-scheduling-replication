935d5e558ea7ed804f0a5bee0c92df5420a1a87c
anthonybrown0528 <anthonybrown0528@protonmail.com>
2026-02-24 02:11:43 -0500

    Fix bug in uunifast implementation

--- generate_task_sets.py
+++ generate_task_sets.py
@@ -14,12 +14,9 @@ def generate_task_sets(num_tasks: int, target_utilization: float) -> np.array:
     s[0] = 0
     s[num_tasks] = target_utilization
 
-    for i in range(num_tasks - 1, 0):
-        scaler = r[i] ** (1.0 / i)
+    for i in range(num_tasks - 1, 0, -1):
+        scaler = r[i - 1] ** (1.0 / i)
         s[i] = s[i + 1] * scaler
 
     task_utilizations = s[1: num_tasks + 1] - s[0: num_tasks]
     return task_utilizations
-
-if __name__ == '__main__':
-    generate_task_sets()
