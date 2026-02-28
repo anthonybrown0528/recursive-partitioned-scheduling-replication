db329222da91c861a1d7bd30de94861e52d82b4e
anthonybrown0528 <anthonybrown0528@protonmail.com>
2026-02-28 12:59:23 -0500

    Discover error using generated task sets

--- test_recursive_gang_scheduler.py
+++ test_recursive_gang_scheduler.py
@@ -1,6 +1,8 @@
 from recursive_gang_scheduler import recursive_gang_schedule
 from recursive_gang_scheduler import Task
 
+import pickle
+
 def test_valid_schedule():
     t1 = Task(2, 1, 2, 5, 2)
     t2 = Task(5, 1, 5, 5, 5)
@@ -22,3 +24,10 @@ def test_known_partition():
     assert schedulable
 
     assert len(forest.leaves()) == 3
+
+def test_8407():
+    obj = None
+    with open('dump.pkl', 'rb') as file:
+        obj = pickle.load(file)
+        obj = pickle.loads(obj)
+    recursive_gang_schedule(obj, 8)
