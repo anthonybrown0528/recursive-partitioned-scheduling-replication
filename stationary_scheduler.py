9b6accb9203fbbae8003c6943be1de4a6d789a4e
anthonybrown0528 <anthonybrown0528@protonmail.com>
2026-03-05 20:06:24 -0500

    fix index error in stationary schedulability test

--- stationary_scheduler.py
+++ stationary_scheduler.py
@@ -10,7 +10,7 @@ processor_assignments = {}
 suspension_inducer_sets = {}
 response_bounds = {}
 
-taskset = []
+tasklist = []
 
 def compute_suspension_inducing_set(i: int, k: int) -> set[int]:
     suspension_inducer_set = set()
@@ -32,13 +32,13 @@ def compute_interference_set(k: int, mk: int, m: int) -> set[int]:
 def transform_interference_set(k: int):
     transformed_interference_set = set()
     for i in interference_sets[k]:
-        task = taskset[i]
-        s = task.r - task.c
+        task = tasklist[i]
+        s = response_bounds[i] - task.c
 
         option = 0
         for j in suspension_inducer_sets[(i, k)]:
             term = 1
-            term = term + math.ceil(task.r / taskset[j].r)
+            term = term + math.ceil(response_bounds[i] / tasklist[j].r)
 
             term = term * task.c
             option = option + term
@@ -48,31 +48,33 @@ def transform_interference_set(k: int):
     return transformed_interference_set
 
 def is_schedulable(k: int, transformed_interference_set: set[int]):
-    task = taskset[k]
 
-    response_bound_old = task.c
-    response_bound_new = 0
+    task = tasklist[k]
+    if len(transformed_interference_set) == 0:
+        response_bounds[k] = task.c
+        return True
+
+    response_bound_old = 0
+    response_bound_new = task.c
 
     transformed_interference_list = sorted(list(transformed_interference_set))
 
     x = np.zeros(len(transformed_interference_list))
 
-    it = enumerate(transformed_interference_list)
-    (idx, (i, s)) = next(it)
+    (i, s) = transformed_interference_list[0]
+    q_values = [s * x[0]]
 
-    q_values = [s * x[idx]]
-
-    for idx, (i, s) in it:
+    for idx, (i, s) in enumerate(transformed_interference_list[1:]):
         val = q_values[-1]
         val = val + s * x[idx]
         q_values.append(val)
 
-    while response_bound_new != response_bound_old and response_bound_new < task.d:
-        response_bound_new = 0
-        for (i, _) in transformed_interference_list:
-            interfering_task = taskset[i]
+    while response_bound_new != response_bound_old and response_bound_new <= task.d:
+        response_bound_old = response_bound_new
+        for idx, (i, _) in enumerate(transformed_interference_list):
+            interfering_task = tasklist[i]
 
-            term = response_bound_old + q_values[i] + (1 - x[i]) * (response_bounds[i] - interfering_task.c)
+            term = response_bound_old + q_values[idx] + (1 - x[idx]) * (response_bounds[i] - interfering_task.c)
             term = math.ceil(term / interfering_task.period)
 
             response_bound_new = response_bound_new + term
@@ -83,23 +85,28 @@ def is_schedulable(k: int, transformed_interference_set: set[int]):
 
 def assign_processors(k: int, j: int, m: int):
     assignment = set()
-    task = taskset[k]
+    task = tasklist[k]
     for i in range(j, j + task.m):
         assignment.add(i % m)
     processor_assignments[k] = assignment
+
 def stationary_schedule(taskset: list[Task], m: int):
+    global tasklist
+
     sorted_tasks = sorted(taskset)
-    for k, task in enumerate(sorted_tasks):
+    tasklist = sorted_tasks
+    for k, _ in enumerate(sorted_tasks):
         schedulable = False
         for j in range(m):
-            assign_processors(task, j)
-            interference_set = compute_interference_set(task.m, j, m)
+            assign_processors(k, j, m)
+            interference_set = compute_interference_set(k, j, m)
             for i in interference_set:
                 compute_suspension_inducing_set(i, k)
             interference_set = transform_interference_set(k)
 
-            if is_schedulable(task, interference_set):
+            if is_schedulable(k, interference_set):
                 schedulable = True
                 break
         if not schedulable:
             return False
+    return True
