9b4233d23ab90fe1847bb910f0da2991e8e9352e
anthonybrown0528 <anthonybrown0528@protonmail.com>
2026-05-01 18:05:44 -0400

    Add code to more easily see how RPS-FP creates partitions

--- script/stress_tests.py
+++ script/stress_tests.py
@@ -8,13 +8,15 @@ from src.task import Task
 from src.recursive_gang_scheduler import recursive_gang_schedule
 
 taskset1 = [
-    [5, 1, 1, 4],
-    [3, 1, 2, 4],
-    [2, 1, 3, 4],
-    [2, 1, 3, 4],
+    [3, 1, 1, 4],
+    [2, 1, 1, 4],
+    [2, 1, 2, 4],
+    [2, 1, 2, 4],
     [2, 1, 4, 4],
     [2, 1, 4, 4],
-    [2, 1, 4, 4]
+    [2, 1, 4, 4],
+    [2, 1, 4, 4],
+    [1, 1, 2, 4],
 ]
 
 taskset2 = [
@@ -31,7 +33,7 @@ def create_taskset(tasks: list):
 
     for entry in tasks:
         m, c, d, p = entry
-        taskset.append(Task(m, c, d, p, p))
+        taskset.append(Task(m, c, d, p, d))
     return taskset
 
 
@@ -42,9 +44,22 @@ def process_data(taskset, m, use_sp):
         success, forest, num_scheduled_tasks = recursive_gang_schedule(taskset, m, use_sp=True)
 
     n = len(taskset)
-    return success, n, m, num_scheduled_tasks
+    return success, n, m, num_scheduled_tasks, forest
 
-success, _, _, _ = process_data(create_taskset(taskset1), 4, False)
+def print_forest_details(forest):
+    print('=' * 40)
+    for leaf, tree in forest.leaves():
+
+        task_id = list(map(lambda x: (x.id, x.m, x.c, x.d), leaf.tasks))
+        print(task_id, leaf.m)
+        print('=' * 40)
+
+    print('-' * 40)
+
+success, _, _, _, forest = process_data(create_taskset(taskset1), 5, False)
 print(success)
-success, _, _, _ = process_data(create_taskset(taskset2), 4, False)
+print_forest_details(forest)
+
+success, _, _, _, forest = process_data(create_taskset(taskset2), 4, False)
 print(success)
+print_forest_details(forest)
